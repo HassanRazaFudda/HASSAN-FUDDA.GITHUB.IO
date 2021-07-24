65d604b10f3e5ba34860fce695e947cbe86b6bd5
Ahmed-Raza <ahmedrazay.123@gmail.com>
2021-07-25 01:37:13 +0500

    Object-practice

---
+++ object-Assignment/question#2/app.js
@@ -0,0 +1,10 @@
+var obj = {
+    name:"Hassan Raza",
+    email:"hraza66789@gmail.com",
+    password:"123456",
+    age:"18",
+    gender:"male",
+    city:"Karachi",
+    country:"Pakistan"
+}
+document.write((Boolean(obj.age)) + " " + (Boolean(obj.country))+ " " +(Boolean(obj.FirstName)) + " " + (Boolean(obj.LastName)));
